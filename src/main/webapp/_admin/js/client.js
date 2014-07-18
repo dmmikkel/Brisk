38c6152e422e434ed37f79b05a1ed3638e53d46d
dmmikkel <dmi@enonic.com>
2014-07-18 13:20:06 +0200

    Cleaned up js client

--- src/main/webapp/_admin/js/client.js
+++ src/main/webapp/_admin/js/client.js
@@ -1,8 +1,9 @@
 var BriskClient = (function($, undefined){
 
     function BriskClient(options) {
-        this.site = options.site || "";
-        this.base = options.base || "/_api";
+        var o = options || {};
+        this.site = o.site || "";
+        this.base = o.base || "/_api";
     }
 
     BriskClient.prototype.getPages = function(callback) {
@@ -26,54 +27,4 @@ var BriskClient = (function($, undefined){
 
 })(jQuery);
 
-/* Data binding */
-(function(BriskBinding, $, undefined){
-
-    var briskClient = new BriskClient({});
-
-    var pages;
-
-    var objectPageListeners;
-    var eachPageListeners;
-
-    $(function(){
-        objectPageListeners = $("*[data-brisk-object='page']");
-        eachPageListeners = $("*[data-brisk-each='page']");
-
-        loadPages();
-    });
-
-    BriskBinding.setPages = function(p) {
-        pages = p;
-        updatePages();
-    };
-
-    function loadPages() {
-        briskClient.getPages(function(json){
-            pages = json;
-            updatePages();
-        });
-    }
-
-    function updatePages() {
-        for (var i = 0; i < pages.length; i++) {
-            var page = pages[i];
-            var objectListeners = objectPageListeners.filter(function(){
-                return $(this).data("brisk-key") == page.key
-            });
-            mapObject(page, objectListeners);
-
-        }
-    }
-
-    function mapObject(object, parents) {
-        parents.find("*[data-brisk-property]").each(function(){
-            var self = $(this);
-            if (self.is("input")
-                self.val(object[self.data("brisk-property")]);
-            else
-                self.text(object[self.data("brisk-property")]))
-        });
-    }
-
-})(window.BriskBinding = window.BriskBinding || {}, jQuery);
+var client = new BriskClient();
